fddfeafa4e731bdf5e9420ebfe3413b0351ca48b
Osinachi Omeh <osi5840@gmail.com>
2025-03-18 13:27:18 -0500

    Implement calculateRentalCost function

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,27 @@
 /**
- * @param {number} days
+ * Calculates the total rental cost based on the number of days.
  *
- * @return {number}
+ * @param {number} days - Number of days the car is rented
+ * @return {number} - Total cost after applying discounts
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAILY_RATE = 40;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+
+  const totalCost = days * DAILY_RATE;
+
+  if (days >= LONG_TERM) {
+    return totalCost - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return totalCost - SHORT_TERM_DISCOUNT;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
